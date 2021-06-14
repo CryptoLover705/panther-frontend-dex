bdabbb6af7a0a9dbb01c2abe21c35957aa48d9b8
CryptoLover705 <50221455+CryptoLover705@users.noreply.github.com>
2021-06-14 22:01:06 +0100

    minor update

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -25,61 +25,61 @@ const config: MenuEntry[] = [
       // },
     ],
   },
-  {
-    label: 'Farms',
-    icon: 'FarmIcon',
-    href: 'https://pantherswap.com/farms',
-  },
-  {
-    label: 'Pools',
-    icon: 'PoolIcon',
-    href: 'https://pantherswap.com/pools',
-  },
-  {
-    label: 'Jungles',
-    icon: 'JungleIcon',
-    href: 'https://pantherswap.com/jungles',
-  },
-  {
-    label: 'Lottery',
-    icon: 'TicketIcon',
-    href: 'https://pantherswap.com/lottery',
-  },
-  {
-    label: 'IPO',
-    icon: 'IfoIcon',
-    href: 'https://pantherswap.com/ipo',
-  },
-  {
-    label: 'Referrals',
-    icon: 'ReferralIcon',
-    href: 'https://pantherswap.com/referrals',
-  },
-  {
-    label: 'Audits',
-    icon: 'ShieldIcon',
-    href: 'https://docs.pantherswap.com/security/audits',
-  },
+  // {
+  //   label: 'Farms',
+  //   icon: 'FarmIcon',
+  //   href: 'https://pantherswap.com/farms',
+  // },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: 'https://pantherswap.com/pools',
+  // },
+  // {
+  //   label: 'Jungles',
+  //   icon: 'JungleIcon',
+  //   href: 'https://pantherswap.com/jungles',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: 'https://pantherswap.com/lottery',
+  // },
+  // {
+  //   label: 'IPO',
+  //   icon: 'IfoIcon',
+  //   href: 'https://pantherswap.com/ipo',
+  // },
+  // {
+  //   label: 'Referrals',
+  //   icon: 'ReferralIcon',
+  //   href: 'https://pantherswap.com/referrals',
+  // },
+  // {
+  //   label: 'Audits',
+  //   icon: 'ShieldIcon',
+  //   href: 'https://docs.pantherswap.com/security/audits',
+  // },
   {
     label: 'Features',
     icon: 'FeatureIcon',
     items: [
-      {
-        label: 'Automatic LP',
-        href: 'https://docs.pantherswap.com/tokenomics/automatic-liquidity',
-      },
-      {
-        label: 'Automatic Burning',
-        href: 'https://docs.pantherswap.com/tokenomics/automatic-burning',
-      },
-      {
-        label: 'Harvest Lockup',
-        href: 'https://docs.pantherswap.com/tokenomics/harvest-lockup',
-      },
-      {
-        label: 'Anti-Whale',
-        href: 'https://docs.pantherswap.com/tokenomics/anti-whale',
-      },
+      // {
+      //   label: 'Automatic LP',
+      //   href: 'https://docs.pantherswap.com/tokenomics/automatic-liquidity',
+      // },
+      // {
+      //   label: 'Automatic Burning',
+      //   href: 'https://docs.pantherswap.com/tokenomics/automatic-burning',
+      // },
+      // {
+      //   label: 'Harvest Lockup',
+      //   href: 'https://docs.pantherswap.com/tokenomics/harvest-lockup',
+      // },
+      // {
+      //   label: 'Anti-Whale',
+      //   href: 'https://docs.pantherswap.com/tokenomics/anti-whale',
+      // },
     ],
   },
   // {
@@ -111,27 +111,27 @@ const config: MenuEntry[] = [
       {
         label: 'BscScan',
         href: 'https://bscscan.com/token/0x1f546ad641b56b86fd9dceac473d1c7a357276b7',
-      },
-      {
-        label: 'DappRadar',
-        href: 'https://dappradar.com/binance-smart-chain/defi/pantherswap',
-      },
-      {
-        label: 'CoinGecko',
-        href: 'https://www.coingecko.com/en/coins/pantherswap',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/pantherswap/',
-      },
-      {
-        label: 'LiveCoinWatch',
-        href: 'https://www.livecoinwatch.com/price/PantherSwap-PANTHER',
-      },
-      {
-        label: 'Vfat',
-        href: 'https://vfat.tools/bsc/panther',
-      },
+      }
+      // {
+      //   label: 'DappRadar',
+      //   href: 'https://dappradar.com/binance-smart-chain/defi/pantherswap',
+      // },
+      // {
+      //   label: 'CoinGecko',
+      //   href: 'https://www.coingecko.com/en/coins/pantherswap',
+      // },
+      // {
+      //   label: 'CoinMarketCap',
+      //   href: 'https://coinmarketcap.com/currencies/pantherswap/',
+      // },
+      // {
+      //   label: 'LiveCoinWatch',
+      //   href: 'https://www.livecoinwatch.com/price/PantherSwap-PANTHER',
+      // },
+      // {
+      //   label: 'Vfat',
+      //   href: 'https://vfat.tools/bsc/panther',
+      // },
     ],
   },
   // {
--- src/components/VersionBar/index.tsx
+++ src/components/VersionBar/index.tsx
@@ -22,18 +22,18 @@ const VersionBar = () => {
   return (
     <Wrapper>
       <Text bold mr="16px">
-        You are using PantherSwap AMM exchange.
+        You are using CryptoSwap AMM exchange.
       </Text>
-      <Button
-        variant="subtle"
-        as={Link}
-        href="https://docs.pantherswap.com/amm-launch"
-        endIcon={<HelpIcon color="white" />}
-        size="sm"
-        ml="16px"
-      >
-        LP Migration
-      </Button>
+      // <Button
+      //   variant="subtle"
+      //   as={Link}
+      //   href="https://docs.pantherswap.com/amm-launch"
+      //   endIcon={<HelpIcon color="white" />}
+      //   size="sm"
+      //   ml="16px"
+      // >
+      //   LP Migration
+      // </Button>
     </Wrapper>
   )
 }
